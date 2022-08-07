faee63751efe7410b224a6035597faa29ba0cf4e
Matt Allen <33626681+mataln@users.noreply.github.com>
2022-08-07 15:57:50 +0100

    Removed smooth-loss not written by me (recommit)

--- utils/utils.py
+++ utils/utils.py
@@ -319,23 +319,3 @@ def plot_depth_images(depth_images, nrows=2, figsize=None):
                 ax[i].imshow(depth_images[i], origin='lower')
         
     return fig, ax    
-
-def smooth_loss(pred, gold, smoothing=True):
-    """Calculate cross entropy loss, apply label smoothing if needed.
-    This isn't used any more, since torch now supports label smoothing."""
-
-    gold = gold.contiguous().view(-1)
-
-    if smoothing:
-        eps = 0.2
-        n_class = pred.size(1)
-
-        one_hot = torch.zeros_like(pred).scatter(1, gold.view(-1, 1), 1)
-        one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
-        log_prb = F.log_softmax(pred, dim=1)
-
-        loss = -(one_hot * log_prb).sum(dim=1).mean()
-    else:
-        loss = F.cross_entropy(pred, gold, reduction='mean')
-
-    return loss
